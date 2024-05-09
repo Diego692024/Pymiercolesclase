4ddc920b037b20dccf25aebafac136c380ac1319
Diego692024 <168148627+Diego692024@users.noreply.github.com>
2024-05-08 21:10:24 -0400

    Add files via upload

---
+++ bass.py
@@ -0,0 +1,21 @@
+
+def porcentaje2 (numero, porcentaje):
+    resultado = numero * (porcentaje / 100)
+    return resultado
+
+# funcion que calcule un descuento
+# si el precio es mayor a 100 entonces el descuento es del 15%
+# si es menor a 100 entonces 5%
+def descuento(precio):
+    descuento = 0
+    if precio > 100:
+        descuento=porcentaje2(precio, 15)
+    else:
+        descuento=porcentaje2(precio, 5)
+    return descuento
+#copiado de funciones 4
+
+# https://codeshare.io/ezMRor investigar como se usa, para dos personas por una url
+
+
+
